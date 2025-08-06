8f0a30011306648450dd0ced2b12f8016e354b64
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2025-08-06 11:15:12 +0000

    Anstraction demonstrated

---
+++ Abstraction/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Abstract Classes and Methods Demo ---");
+            
+            Circle myCircle = new Circle(5);
+            Rectangle myRectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of circle with radius 5: {myCircle.GetArea()}");
+            Console.WriteLine($"Area of rectangle 4x6: {myRectangle.GetArea()}");
+        }
+    }
+
+    // Abstract base class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Derived class
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    // Derived class
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public override double GetArea()
+        {
+            return length * width;
+        }
+    }
+}
