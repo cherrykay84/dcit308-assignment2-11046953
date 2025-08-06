97336b5f9acccb8d70f464c011e066e4a3546a28
cherrykay84 <ssumaila009@st.ug.edu.gh>
2025-08-06 11:29:48 +0000

    Demonstrated Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Interface Demo ---");
+            
+            Car myCar = new Car();
+            Bicycle myBike = new Bicycle();
+
+            Console.WriteLine("Car action: ");
+            myCar.Move();
+            
+            Console.WriteLine("Bicycle action: ");
+            myBike.Move();
+        }
+    }
+
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Implementing class
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Implementing class
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
